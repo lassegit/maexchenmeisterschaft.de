c9a32e75f200170a0119be736549f738a55a3bb0
Lasse <lassetange@gmail.com>
2019-06-22 14:25:33 +0200

    Adding styled comp

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site, e.g. src/images/gatsby-icon.png
       },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
-import Image from '../components/image';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -11,9 +9,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
